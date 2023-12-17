5d9e5ab33aeee55484c459dfab6831ebb8cf07ef
pkm0707 <mpraveenkumar@student.tce.edu>
2023-12-17 18:01:19 +0530

    deleted

--- InbuildPackages/fs.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs")
-let quotes = "Dont stop until you done !"
-fs.writeFile("quotes.pdf",quotes,()=>{
-    console.log("writing completed")
-})
-fs.readFile("")
--- InbuildPackages/test.js
+++
@@ -1,8 +0,0 @@
-// console.log("hello, welcome to the world of mark antony mamae !")
-// console.log(global)
-console.log(process.argv)
-const[,,n1,n2] = process.argv
-function multiply(n1,n2){
-    console.log(+n1 * +n2);
-}
-multiply(n1,n2)
